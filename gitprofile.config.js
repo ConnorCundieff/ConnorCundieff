d6c1e52f73f12bf983700ae468a05f923540779c
brenda-l-killingsworth <147526709+brenda-l-killingsworth@users.noreply.github.com>
2023-10-10 17:46:44 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1QC1mIwxuM7DYo-1xFBVFtyyn9dwxfpjn/view?usp=drive_link', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1QC1mIwxuM7DYo-1xFBVFtyyn9dwxfpjn/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
